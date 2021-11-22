a0c37b73c8300a0c8f124273223c934f7c67707d
AbdelrahmanXMohamed <abdelrahman.mohamedx1999@gmail.com>
2021-11-22 18:34:27 +0200

    interval

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,30 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/api/all_posts")
-        .then((response) => response.json())
-        .then((data) => {
-            let postCard = data.map(
-                (item) =>
-                    `
+    document.querySelector("#AllPosts").click();
+
+});
+var interval;
+document.querySelector("#AllPosts").onclick = (e) => {
+    e.preventDefault()
+
+    document.querySelector("#app").innerHTML = `<form class="allPosts">
+        <div class="posts">
+            <textarea class="d-block" required> </textarea>
+            <input class="btn btn-primary" type="submit" value="Post" />
+        </div>
+        </form>`;
+
+    document.forms[0].onsubmit = (e) => {
+        console.log(e);
+        e.preventDefault();
+    }
+    console.log(x);
+    interval = setInterval(function () {
+        fetch("/api/all_posts")
+            .then((response) => response.json())
+            .then((data) => {
+                let postCard = data.map(
+                    (item) =>
+                        `
     <div class="posts" id="post${item.id}">
       <p class="creator">${item.creator}</p>
       <p class="date">${item.created_at}</p>
       <p class="body">${item.post_body}</p>
 
       <img onclick="like(${item.id})" class=${item.likes.some(
-                        (x) => false || x === document.querySelector("strong").innerHTML
-                    )
-                        ? "liked"
-                        : "notLiked"
-                    } src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-like-notifications-justicon-flat-justicon.png"/>
+                            (x) => false || x === document.querySelector("strong").innerHTML
+                        )
+                            ? "liked"
+                            : "notLiked"
+                        } src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-like-notifications-justicon-flat-justicon.png"/>
       <p>Likes ${item.likes.length}</p>
     </div>`
-            );
+                );
+                if (document.querySelector("#allPosts"))
+                    document.querySelector("#allPosts").innerHTML = postCard
+                else
+                    document.querySelector("#app").innerHTML += `<div id="allPosts" class="allPosts">${postCard}</div>`;
 
-            document.querySelector(
-                "#app"
-            ).innerHTML += `<div class="allPosts">${postCard}</div>`;
-        });
-});
+            })
+    }, 1000);
+
+};
 document.querySelector("#Profile").onclick = (e) => {
     fetch("/api/profile")
         .then((response) => response.json())
@@ -73,6 +96,7 @@ document.querySelector("#Profile").onclick = (e) => {
         });
 };
 document.querySelector("#Following").onclick = (e) => {
+    clearInterval(interval)
     fetch("/api/following")
         .then((response) => response.json())
         .then((data) => {
@@ -105,23 +129,35 @@ document.querySelector("#Following").onclick = (e) => {
         });
 };
 like = (id) => {
-    fetch(`/api/posts`).then(response => response.json()).then(data => {
-        fetch(`/api/posts/${id}`, {
-            method: "PUT",
-            body: JSON.stringify({ ...data })
-        }).then(response => response.json()).then(function (data) {
-            document.querySelector(`#post${id}`).innerHTML = `    <p class="creator">${data.creator}</p>
+    /*
+         “Like” and “Unlike”:
+         Users should be able to click a button or link on any post to toggle whether or not they “like” that post.
+         Using JavaScript, you should asynchronously let the server know to update the like count (as via a call to fetch) and
+         then update the post’s like count displayed on the page, without requiring a reload of the entire page.
+     */
+
+    fetch(`/api/posts`)
+        .then((response) => response.json())
+        .then((data) => {
+            fetch(`/api/posts/${id}`, {
+                method: "PUT",
+                body: JSON.stringify({ ...data }),
+            })
+                .then((response) => response.json())
+                .then(function (data) {
+                    document.querySelector(
+                        `#post${id}`
+                    ).innerHTML = `    <p class="creator">${data.creator}</p>
       <p class="date">${data.created_at}</p>
       <p class="body">${data.post_body}</p>
 
       <img onclick="like(${data.id})" class=${data.likes.some(
-                (x) => false || x === document.querySelector("strong").innerHTML
-            )
-                    ? "liked"
-                    : "notLiked"
-                } src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-like-notifications-justicon-flat-justicon.png"/>
-      <p>Likes ${data.likes.length}</p>`
+                        (x) => false || x === document.querySelector("strong").innerHTML
+                    )
+                            ? "liked"
+                            : "notLiked"
+                        } src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-like-notifications-justicon-flat-justicon.png"/>
+      <p>Likes ${data.likes.length}</p>`;
+                });
         });
-    });
-
-}
+};
